e1ad832227f2edf32b0bf466b4e27f79625868a2
Mathias <mathias.lithen@gmail.com>
2023-11-27 22:43:56 +0200

    minor cleanup

--- get_input.py
+++ get_input.py
@@ -4,19 +4,16 @@ import datetime
 import os
 import subprocess
 
-S = None
-
 
 def main():
-    p = argparse.ArgumentParser(description='Get AOC input')
     n = datetime.datetime.now()
+    p = argparse.ArgumentParser(description='Get AOC input')
     p.add_argument('-y', '--year', type=int, default=n.year)
     p.add_argument('-d', '--day', type=int, default=n.day)
     a = p.parse_args()
-    s = S or os.getenv('AOC_SESSION')
     u = 'https://adventofcode.com/%d/day/%d/input' % (a.year, a.day)
-    c = ' '.join(['curl', u, '--cookie "session=%s"' % s])
-    x = subprocess.check_output(c, shell=True)
+    c = '--cookie "session=%s"' % os.getenv('AOC_SESSION')
+    x = subprocess.check_output(f'curl {u} {c}', shell=True)
     with open('%d.in' % a.day, 'wb') as f:
         f.write(x)
 
