173e70b34936555dcfb414c07911e4bd657ad815
Mathias Lithen <mathias.lithen@gmail.com>
2024-12-03 09:31:21 +0200

    add some imports

--- base.py
+++ base.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
+# ruff: noqa: F401
 import sys
+from collections import Counter, defaultdict, deque
+from heapq import heappop, heappush
+from math import lcm
 
 fn = sys.argv[1] if len(sys.argv) > 1 else 'n.in'
 with open(fn) as f:
