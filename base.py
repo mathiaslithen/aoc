98ffb594f9949ac55c0db9ec0f1886e5d0d81772
Mathias Lithen <mathias.lithen@gmail.com>
2024-12-11 11:43:46 +0200

    use pypy and set higher max recursion limit by default

--- base.py
+++ base.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/pypy3
 # ruff: noqa: F401
 import sys
 from collections import Counter, defaultdict, deque
 from heapq import heappop, heappush
 from math import lcm
 
+sys.setrecursionlimit(10**5)
 fn = sys.argv[1] if len(sys.argv) > 1 else 'n.in'
 with open(fn) as f:
     data = f.read().strip()
-lines = data.split('\n')
+G = data.splitlines()
+R = len(G)
+C = len(G[0])
